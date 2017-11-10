c619f93a06456200bfdc1ad0e4a86eb6aaa39301
dydy <dylanfoster.esgi@gmail.com>
2017-11-10 16:55:08 +0100

    printing google's custom response in dialog if received

--- intentMatcher.js
+++ intentMatcher.js
@@ -1,7 +1,16 @@
+const builder = require('botbuilder');
+
 module.exports.createIntent = (intents, stringIntent) => {
   intents.matches(stringIntent, [
     (session, args) => {
-      session.send(`args: ${JSON.stringify(args)}`);
+      const { entities } = args;
+      if (entities.find(e => e.entity)) {
+        session.send(entities.find(e => e.entity).entity);
+      } else {
+        const matchedRes = builder.EntityRecognizer.findEntity(entities);
+        // const matchedRes = builder.EntityRecognizer.findBestMatch(entities);
+        session.send(`args: ${JSON.stringify(args)} \n\n matchedRes: ${matchedRes}`);
+      }
     },
   ]);
 };
